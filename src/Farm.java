de147a5a8930236ae1bdcec264946b96c955f42f
Dragonosauras <mitchellveale@xtra.co.nz>
2020-04-16 21:33:52 +1200

    javadoc and visibility fixes
    Added javadoc entries to all finalised methods and attributes.
    
    Fixed some attributes that were meant to be declared as protected but were declared as private.
    
    added .gitignore

--- src/Farm.java
+++ src/Farm.java
@@ -1,15 +1,35 @@
 public class Farm {
-    private String farmName;
+    /**
+     * the name of the farm
+     */
+    protected String farmName;
+    /**
+     * the name of the farmer
+     */
     //If we add farmer attributes later we should change this to a 'Farmer' class
-    private String farmerName;
-    private FarmType farmType;
+    protected String farmerName;
+    /**
+     * the type of farm
+     */
+    protected FarmType farmType;
 
+    /**
+     * Instantiates the Farm class
+     * @param farmName the name of the farm
+     * @param farmerName the name of the farmer
+     * @param farmType  the farm type.
+     *                 An array of possible farm types can be obtained with {@link #farmTypes()}
+     */
     public Farm(String farmName, String farmerName, FarmType farmType){
         this.farmName = farmName;
         this.farmerName = farmerName;
         this.farmType = farmType;
     }
 
+    /**
+     * Used to declare all farm types
+     * @return An array of all farm types
+     */
     public static FarmType[] farmTypes(){
         return new FarmType[]{
                 new FarmType("Farm type 1"),
--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,20 @@
 public class Game extends Farm {
+    /**
+     * the length of the game in days
+     */
     private int gameLength;
 
+    /**
+     * Instatiates {@link Game}
+     * @param gameLength the length of the game in days
+     * @param farmName the name of the farm
+     * @param farmerName the name of the farmer
+     * @param farmType  the farm type.
+     *                  An array of possible farm types can be obtained with {@link #farmTypes()}
+     */
     public Game(int gameLength, String farmName, String farmerName, FarmType farmType) {
         super(farmName, farmerName, farmType);
         this.gameLength = gameLength;
     }
+
 }

9fe28ba8fe8acbecea739243cb918fcd6f0b2bf6
rdb80 <63752409+rdb80@users.noreply.github.com>
2020-04-21 10:41:29 +1200

    Update runTextGame.java

--- src/runTextGame.java
+++ src/runTextGame.java
@@ -1,15 +1,20 @@
 
 import java.util.Scanner;  // Import the Scanner class
 
+
 public class runTextGame {
 	
 	Scanner scan = new Scanner(System.in);  // Create a Scanner object
 	int gameLength = 0;
-	String farmerName = "x";
+	String farmerName;
 	int farmType = 0;
 	String farmName;
+	//Farm userFarm;
 	
 	public void setupGame() {
+		
+		// Gets gameLength, farmerName, farmType, farmName and initializes userFarm
+		
 		do {
 			System.out.println("Enter valid game length (Between 5-10 days): \n");
 			gameLength = scan.nextInt();  // Read user input
@@ -21,17 +26,18 @@ public class runTextGame {
 			System.out.println("Enter Valid Farmer name (3-15 characters, no numbers/characters) \n");
 			farmerName = scan.nextLine();
 		}
-		while(farmerName.length() < 3 || farmerName.length() > 15);    // TODO find a is-alpha method 
+		while(farmerName.length() < 3 || farmerName.length() > 15);  // TODO find a is-alpha method & maybe change to exception
 		
 		
 		do {
-		System.out.println(" 1 : Crop-Master");            // examples for now 
-		System.out.println(" 2 : animal-whisperer");
-		System.out.println(" 3 : Money");
-		System.out.println(" 4 : lucky \n");
-		farmType = scan.nextInt(); 
-		}
-		while(farmType < 0 || farmType > 4); 
+			System.out.println("*** Input the number that corresponds to the action you would like to do ***");
+			System.out.println(" 1 : Crop-Master");            // examples for now 
+			System.out.println(" 2 : Animal-whisperer");
+			System.out.println(" 3 : Money");
+			System.out.println(" 4 : Lucky \n");
+			farmType = scan.nextInt(); 
+			}
+			while(farmType < 0 || farmType > 4); 
 		
 		
 		do {
@@ -40,11 +46,14 @@ public class runTextGame {
 		}
 		while(farmName.length()< 1);
 		
-		//Farm userFarm = new Farm(farmName, farmerName, farmType);
+		// userFarm = new Farm(farmName, farmerName, farmType);
 	}
 
-	void play(){
+	public void playMenu(){
 		
+		int doCommand = 0;
+		do {
+
 		System.out.println("Day x begins! \n");
 		System.out.println("Day : ");
 		System.out.println("Actions Remaining : ");
@@ -56,8 +65,41 @@ public class runTextGame {
 		System.out.println("5 : Harvest Crops");
 		System.out.println("6 : Tend to Farm Land" );
 		System.out.println("7 : Next Day\n");
-		System.out.println("Input the Number that corresponds to the action");
+		System.out.println("*** Input the number that corresponds to the action you would like to do ***");
 		
+		doCommand= scan.nextInt();
+		}
+		while(doCommand < 1 || doCommand > 7);          // would like a error message for invalid input 
+
+		switch(doCommand) {
+		
+		case 1: 
+			// open store page
+			storeMenu();
+			break;
+		case 2:
+			// tend crops
+			break;
+		case 3: 
+			// Feed animals
+			break;
+		case 4:
+			//Play with animals
+			break;
+		case 5:
+			// harvest crops
+			break;
+		case 6:
+			// tend to farm
+			break;
+		case 7:
+			// next day
+			break;
+		}
+	}
+	
+	void storeMenu() {
+		System.out.println("Welcome to the store!");
 	}
 	
 	
@@ -65,7 +107,7 @@ public class runTextGame {
 		
 		runTextGame game = new runTextGame();  
 		game.setupGame();
-		game.play();
+		game.playMenu();
 		
 	}
 	
